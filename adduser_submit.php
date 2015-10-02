96e0cdd400c57259a91f2eb817f04b352cd9fae5
Kareem <kareem.tarek04@gmail.com>
2015-10-02 23:00:27 +0200

    altered login_submit , adduser_submit , index.php

--- adduser_submit.php
+++ adduser_submit.php
@@ -12,41 +12,56 @@ require("includes/connection.php");
 if(!isset( $_POST['username'], $_POST['password'], $_POST['form_token']))
 {
     $message = 'Please enter a valid username and password';
+    $url="index.php?page=adduser&message=$message";
+    header('Location: '.$url);
 }
 /*** check the form token is valid ***/
 elseif( $_POST['form_token'] != $_SESSION['form_token'])
 {
     $message = 'Invalid form submission';
+    $url="index.php?page=adduser&message=$message";
+    header('Location: '.$url);
 }
 /*** check the username is the correct length ***/
 elseif (strlen( $_POST['username']) > 20 || strlen($_POST['username']) < 4)
 {
     $message = 'Incorrect Length for Username';
+    $url="index.php?page=adduser&message=$message";
+    header('Location: '.$url);
 }
 /*** check the password is the correct length ***/
 elseif (strlen( $_POST['password']) > 20 || strlen($_POST['password']) < 4)
 {
     $message = 'Incorrect Length for Password';
+    $url="index.php?page=adduser&message=$message";
+    header('Location: '.$url);
 }
 /*** check the username has only alpha numeric characters ***/
 elseif (ctype_alnum($_POST['username']) != true)
 {
     /*** if there is no match ***/
     $message = "Username must be alpha numeric";
+    $url="index.php?page=adduser&message=$message";
+    header('Location: '.$url);
+
 }
 /*** check the password has only alpha numeric characters ***/
 elseif (ctype_alnum($_POST['password']) != true)
 {
         /*** if there is no match ***/
         $message = "Password must be alpha numeric";
+        $url="index.php?page=adduser&message=$message";
+        header('Location: '.$url);
 }
 elseif (!($_POST['password'] == $_POST['conpass']))
 {
     $message = "Passwords dont match";
+    $url="index.php?page=adduser&message=$message";
+    header('Location: '.$url);
+
 }
 else
 {   
-
     /*** if we are here the data is valid and we can insert it into database ***/
     $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
@@ -55,35 +70,36 @@ else
     $sql = "INSERT INTO `eShop`.`Users` (`id`, `firstname`, `lastname`, `password`, `email`) VALUES
         (NULL, '$username','$lastname','$password', '$email');";
     mysql_query($sql);   
-    $sql_cond="SELECT * FROM Users order by desc limit 1";
+    $sql_cond="SELECT * FROM Users order by id desc limit 1";
     $query_cond=mysql_query($sql_cond);
     if(mysql_num_rows($query_cond)!=0) {
         $row_cond=mysql_fetch_assoc($query_cond);
+        $_SESSION['user_id'] = $row_cond['id'];
+        $_SESSION['username'] =  $row_cond['firstname'];;
+        $url = "http://localhost/eShop/index.php";
+        header('Location: '.$url);
     }
-    $
 
-
-
-
-
-
-
-
-
-
-
-
-
+    else{
+        $message = "invalid  details";
+        $url="index.php?page=adduser&message=$message";
+        header('Location: '.$url);
     }
+}
    
 ?>
 
+
+
+
+
+
 <html>
-<head>
-<title>Login</title>
-</head>
-<body>
-<p><?php echo $message; ?>
-<a href="index.php?page=adduser">try again</a>
-</body>
+    <head>
+    <title>Register</title>
+    </head>
+    <body>
+    <p><?php echo $message; ?>
+    </body>
 </html>
+
